d830f9f7c86e1088087867132a7643746ee923e5
Bruno Nishimoto <bruno.nishimoto@taqtile.com>
2017-05-04 17:05:39 -0300

    Bruno - Detalhar

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -1,5 +1,5 @@
 import {Router, Request, Response, NextFunction} from 'express';
-const Users = require('../users');
+const Users : Array<any> = require('../users');
 
 export class UserRouter {
   router: Router
@@ -17,10 +17,31 @@ export class UserRouter {
    */
   public getAll(req: Request, res: Response, next: NextFunction) {
     let allUser: string = "";
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < Users.length; i++) {
         allUser += Users[i].name + "\n";
     }
      res.send(allUser);
+  }
+
+  /**
+   * GET details of one User by id
+   */
+  public getOne(req: Request, res: Response, next: NextFunction) {
+    let query = parseInt(req.params.id);
+    let user = Users.find(user => user.id === query);
+    if (user) {
+        res.status(200)
+        .send("name: " + user.name + "\n" + 
+              "email: " + user.email + "\n" + 
+              "password: " + user.password + "\n");
+    }
+    else {
+        res.status(404)
+        .send({
+           message: 'No user found with the given id.',
+           status: res.status
+        });
+    }
   }
 
 
@@ -30,6 +51,7 @@ export class UserRouter {
    */
   init() {
     this.router.get('/', this.getAll);
+    this.router.get('/:id', this.getOne);
   }
 
 }
