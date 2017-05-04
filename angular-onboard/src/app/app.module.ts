1b8cf4454a18b912e964272a3c49d299dc014a79
davicorreiajr <taqtile@Dave.local>
2017-05-04 19:05:29 -0300

    Created login form

--- angular-onboard/src/app/app.module.ts
+++ angular-onboard/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LoginComponent
+    LoginComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
--- angular-onboard/src/app/login/login.component.ts
+++ angular-onboard/src/app/login/login.component.ts
@@ -1,11 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserComponent } from '../user/user.component'
+
 @Component({
-  selector: 'app-login',
+  selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  usuario: string;
+  senha: string;
+  
+  model = new UserComponent(0, "", "", "");
+  submitted = false;
+  onSubmit() { this.submitted = true; }
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.model); }
+
+  newUser() {
+    this.model = new UserComponent(42, '', '', '');
+  }
+
+  setAuth(usuario: string, senha: string) {
+    this.usuario = usuario;
+    this.senha = usuario;
+  }
+
+  
 
   constructor() { }
 
