560b6369230b31c2dfe2369afc8c9ce9d8295c9e
pypasquao <pypasqualini@gmail.com>
2017-05-08 17:29:18 -0300

    Started detail user

---
+++ ProjetoBasico/ProjetoBasico/DetalUserViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DetalUserViewController.swift
+//  ProjetoBasico
+//
+//  Created by Taqtile on 5/8/17.
+//  Copyright © 2017 Taqtile. All rights reserved.
+//
+
+import UIKit
+
+class DetalUserViewController: UIViewController {
+
+    @IBOutlet weak var UserName: UITextField!
+    @IBOutlet weak var UserID: UITextField!
+    @IBOutlet weak var UserEmail: UITextField!
+    @IBOutlet weak var UserCreatedAt: UITextField!
+    @IBOutlet weak var UserUpdatedAt: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
